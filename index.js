a3be9dca8a73818ae241739de1f334883d470d8e
sanisha7 <sanishareddy22@gmail.com>
2019-12-19 13:22:16 +0530

    desiging a basic web page

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT||3000
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/htmls/index.html'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
